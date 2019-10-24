a3a50955e39e5d7e99578bb7058c17c4674411db
paisleypark <2513630371@qq.com>
2019-10-24 22:59:23 +0800

    Adding the successor predecessor release function. P.S. The release function left some bug.

--- chap04/BinarySearchTree/BinarySearchTree.cpp
+++ chap04/BinarySearchTree/BinarySearchTree.cpp
@@ -23,29 +23,15 @@ public:
 	BinaryTree(){};
 	BinaryTree(TYPE _val);
 	int reinit(Node *_new);
+	int transplant(Node *_u, Node *_v);
+	int release();
+	int release(Node *_x);
 
 private:
 	Node *__root = NULL;
 };
 
-class BinarySearchTree : public BinaryTree {
-public:
-	BinarySearchTree(TYPE _val);
-	BinarySearchTree(){};
-	Node *search(TYPE _val);
-	Node *max();
-	const Node *max() const;
-	TYPE max_value() const;
-	Node *min(); 
-	const Node *min() const;
-	TYPE min_value() const;
-	int insert(Node *_new);
-	int insert(const Node *_new);
-	int insert(TYPE _val);
-};
-
 typedef BinaryTree::Node BTNode;
-typedef BinarySearchTree::Node BSTNode;
 
 const BTNode *BinaryTree::getroot() const{
 	return (dynamic_cast<const Node *>(__root));	
@@ -78,6 +64,10 @@ BinaryTree::Node::Node(TYPE _v) {
 	parent = right = left = NULL;
 };
 
+BinaryTree::Node::~Node() {
+	parent = left = right = NULL;
+}; 
+
 int BinaryTree::inorder_walk(const Node *_x) const{
 	if(_x != NULL) {
 	inorder_walk(_x->left);
@@ -102,10 +92,72 @@ BinaryTree::BinaryTree(TYPE _val) {
 	__root->data = _val;
 };
 
-int BinaryTree::reinit(Node *_new) {
+int BinaryTree::reinit(BTNode *_new) {
 	__root = _new;
 	return 0;
 };
+
+int BinaryTree::transplant(BTNode *_u, BTNode *_v) {
+	if(_u == NULL) {
+		std::cout << "Error. Can not transplant to a null" << std::endl;
+		std::exit(-1);
+	}
+	if(_u->parent == NULL) {
+		reinit(_v);
+	}
+	else if(_u == _u->parent->left) {
+		_u->parent->left = _v;
+	}
+	else {
+		_u->parent->right = _v;
+	}
+	_v->parent = _u->parent;
+	release(_u);
+	return 0;
+};
+
+int BinaryTree::release(BTNode *_x) {
+	if(_x != NULL) {
+		release(_x->left);
+		release(_x->right);
+		delete _x;
+	}
+	return 0;
+};
+
+int BinaryTree::release() {
+	release(__root);
+	__root = NULL;
+	return 0;
+};
+
+class BinarySearchTree : public BinaryTree {
+public:
+	BinarySearchTree(TYPE _val);
+	BinarySearchTree(){};
+	Node *search(TYPE _val);
+	Node *max();
+	const Node *max() const;
+	TYPE max_value() const;
+	Node *max(Node *_x);
+	const Node *max(const Node *_x) const;
+	TYPE max_value(const Node *_x) const;
+	Node *min(); 
+	const Node *min() const;
+	TYPE min_value() const;
+	Node *min(Node *_x);
+	const Node *min(const Node *_x) const;
+	TYPE min_value(const Node *_x) const;
+	Node *successor(Node *_x);
+	const Node *successor(const Node *_x) const;
+	Node *predecessor(Node *_x);
+	const Node *predecessor(const Node *_x)  const;
+	int insert(Node *_new);
+	int insert(const Node *_new);
+	int insert(TYPE _val);
+};
+
+typedef BinarySearchTree::Node BSTNode;
 
 BinarySearchTree::BinarySearchTree(TYPE _val) : BinaryTree(_val) {};
 
@@ -165,6 +217,36 @@ TYPE BinarySearchTree::max_value() const{
 	return x->data;
 };
 
+BSTNode *BinarySearchTree::max(BSTNode *_x) {
+	if(_x == NULL) {
+		std::cout << "Error. There is no such node" << std::endl;
+	}	
+	while(_x->right != NULL) {
+		_x = _x->right; 
+	}
+	return _x;
+};
+
+const BSTNode *BinarySearchTree::max(const BSTNode *_x) const{
+	if(_x == NULL) {
+		std::cout << "Error. There is no such node" << std::endl;
+	}	
+	while(_x->right != NULL) {
+		_x = _x->right; 
+	}
+	return _x;
+};
+
+TYPE BinarySearchTree::max_value(const BSTNode *_x) const{
+	if(_x == NULL) {
+		std::cout << "Error. There is no such node" << std::endl;
+	}
+	while(_x->right != NULL) {
+		_x = _x->right; 
+	}
+	return _x->data;
+};
+
 BSTNode *BinarySearchTree::min() {
 	BSTNode *x = getroot();
 	if(x == NULL) {
@@ -196,6 +278,100 @@ TYPE BinarySearchTree::min_value() const{
 		x = x->left;
 	}	
 	return x->data;
+};
+
+BSTNode *BinarySearchTree::min(BSTNode *_x) {
+	if(_x == NULL) {
+		std::cout << "Error. There is no such node" << std::endl;
+	}	
+	while(_x->left != NULL) {
+		_x = _x->left; 
+	}
+	return _x;
+};
+
+const BSTNode *BinarySearchTree::min(const BSTNode *_x) const{
+	if(_x == NULL) {
+		std::cout << "Error. There is no such node" << std::endl;
+	}	
+	while(_x->left != NULL) {
+		_x = _x->left; 
+	}
+	return _x;
+};
+
+TYPE BinarySearchTree::min_value(const BSTNode *_x) const{
+	if(_x == NULL) {
+		std::cout << "Error. There is no such node" << std::endl;
+	}
+	while(_x->left != NULL) {
+		_x = _x->left; 
+	}
+	return _x->data;
+};
+
+BSTNode *BinarySearchTree::successor(BSTNode *_x) {
+	if(_x == NULL) {
+		std::cout << "Error. The node is NULL" << std::endl;
+		return NULL;
+	}
+	if(_x->right != NULL) {
+		return min(_x->right);
+	}
+	BSTNode *y = _x->parent;
+	while(y != NULL && _x == y->right) {
+		_x = y;
+		y = _x->parent;
+	}
+	return y;
+};
+
+const BSTNode *BinarySearchTree::successor(const BSTNode *_x) const{
+	if(_x == NULL) {
+		std::cout << "Error. The node is NULL" << std::endl;
+		return NULL;
+	}
+	if(_x->right != NULL) {
+		return min(_x->right);
+	}
+	const BSTNode *y = _x->parent;
+	while(y != NULL && _x == y->right) {
+		_x = y;
+		y = _x->parent;
+	}
+	return y;
+};
+
+BSTNode *BinarySearchTree::predecessor(BSTNode *_x) {
+	if(_x == NULL) {
+		std::cout << "Error. The node is NULL" << std::endl;
+		return NULL;
+	}
+	if(_x->left != NULL) {
+		return max(_x->left);
+	}
+	BSTNode *y = _x->parent;
+	while(y != NULL && _x == y->left) {
+		_x = y;
+		y = _x->parent;
+	}
+	return y;
+};
+
+const BSTNode *BinarySearchTree::predecessor(const BSTNode *_x) const{
+	if(_x == NULL) {
+		std::cout << "Error. The node is NULL" << std::endl;
+		return NULL;
+	}
+	if(_x->left != NULL) {
+		return min(_x->left);
+	}
+	const BSTNode *y = _x->parent;
+	while(y != NULL && _x == y->left) {
+		_x = y;
+		y = _x->parent;
+	}
+	return y;
 };
 
 int BinarySearchTree::insert(BSTNode *_new) {
@@ -289,9 +465,12 @@ int main(int argc, char *argv[])
 	A.insert(5);
 	A.insert(8);
 	std::cout << std::endl;
+	BSTNode *r = A.getroot();
 	A.inorder_walk();
 	std::cout << A.search(2)->parent->data << std::endl;
 	std::cout << A.min()->data << std::endl;
 	std::cout << A.min_value() << std::endl;
+	A.release(r->left);
+	A.release();
 	return 0;
 }
