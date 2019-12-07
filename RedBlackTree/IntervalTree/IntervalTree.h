0a2c287fa497f42c3f202582224301d367994525
paisleypark <2513630371@qq.com>
2019-12-07 15:56:36 +0800

    add the search function and some annotation, there may be a lot of bugs remained.

--- RedBlackTree/IntervalTree/IntervalTree.h
+++ RedBlackTree/IntervalTree/IntervalTree.h
@@ -14,18 +14,74 @@ public:
 		this->root->max = _y;
 	};
 
+	/**
+	 * Default constructor.
+	 */
 	IntervalTree(){};
 
+	/**
+	 * @brief Right rotation for IntervalTree, fix the max. 
+	 *
+	 * @param _x The pivot
+	 *
+	 * @return 
+	 */
 	int RightRotate(Node *_x);
 
+	/**
+	 * @brief Left rotation for IntervalTree, fix the max. 
+	 *
+	 * @param _x The pivot
+	 *
+	 * @return 
+	 */
 	int LeftRotate(Node *_x);
 
+	/**
+	 * @brief Insert for IntervalTree, fix the max.
+	 *
+	 * @param _new The new node to insert.
+	 *
+	 * @return 
+	 */
 	int insert(Node *_new);
 
+	/**
+	 * @brief Insert for IntervalTree, fix the max.
+	 *
+	 * @param _x The low number of the node to insert
+	 * @param _y The high number of the node to insert
+	 *
+	 * @return 
+	 */
 	int insert(TYPE _x, TYPE _y);
 
+	/**
+	 * @brief Deletion for IntervalTree, fix the max.
+	 *
+	 * @param _d The node to delete.
+	 *
+	 * @return 
+	 */
 	int del(Node *_d);
+
+	/**
+	 * @brief Find the node that overlap with the given interval.
+	 *
+	 * @param _x The low number of the given interval. 
+	 * @param _y The high number of the given interval.
+	 *
+	 * @return 
+	 */
+	Node *IntervalSearch(TYPE _x, TYPE *_y);
 private:
+	/**
+	 * @brief Fixup the max in the tree.
+	 *
+	 * @param _x Start the fixup from node _x.
+	 *
+	 * @return 
+	 */
 	int __IT_Max_Fixup(Node *_x);
 };
 
